9e0704872e162bf1bdf0dd2e67117ea30505ddb5
@DevRafaelSantos <contatodevrafaelsantos@gmail.com>
2025-06-13 07:34:51 -0300

    Atualizar o Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1234566789", "Nokia-1100", "111111111111111", 17);
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("VxCase");
+
+    Iphone iphone = new Iphone("1234566789", "Iphone-11 Pro", "111111111111111", 99);
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Instagram");
+    
