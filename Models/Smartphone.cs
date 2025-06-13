7417739405f0ffd7f787fea32233ed1f0560914f
@DevRafaelSantos <contatodevrafaelsantos@gmail.com>
2025-06-13 07:33:17 -0300

    Atualizar o Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private String Modelo { get; set;}
+        private String IMEI { get; set;}
+        private int Memoria { get; set; }  
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, String modelo, String imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
