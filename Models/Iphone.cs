046c527e7aaf9ffcf63ab9882b7f5bf7e4f5e566
@DevRafaelSantos <contatodevrafaelsantos@gmail.com>
2025-06-13 07:31:01 -0300

    Atualizar o Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
+
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+       // TODO: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone 
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
-    }
+        public Iphone(String numero, String modelo, String imei, int memoria ) : base(numero, modelo, imei, memoria){
+
+        }
+        public override void InstalarAplicativo( String nomeApp){
+           Console.WriteLine($"Instalando {nomeApp} no Iphone");
+        }
+        // TODO: Sobrescrever o método "InstalarApli
 }
